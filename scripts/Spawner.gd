a81b06e0c45cacf5c23a85f6198b85d1ab4ab5cc
Jarflux <ben.oeyen@gmail.com>
2025-01-29 19:57:56 +0100

    Add Crab Spawner, Add Shark, Add Knifem, Reduce Fish spawn

--- scripts/Spawner.gd
+++ scripts/Spawner.gd
@@ -6,11 +6,13 @@ extends Node
 @export var spawn_area_max_Y : float
 @export var fish_or_plastic : int
 @export var flipped : bool
-@export var velocity : Vector2
 @export var spawn_threshold : float # number between 0 and 1
 
 var fish1 = load("res://scenes/prefab/FishExotic.tscn")
 var fish2 = load("res://scenes/prefab/FishClown.tscn")
+var fish3 = load("res://scenes/prefab/FishShark.tscn")
+var crab = load("res://scenes/prefab/Crab.tscn")
+var knife = load("res://scenes/prefab/Knife.tscn")
 var plastic1 = load("res://scenes/prefab/PlasticBottle.tscn")
 var plastic2 = load("res://scenes/prefab/PlasticBeer.tscn")
 var plastic3 = load("res://scenes/prefab/PlasticBag.tscn")
@@ -30,35 +32,47 @@ func _process(float) -> void:
 				spawned = _createPlastic()
 			if fish_or_plastic == 1:
 				spawned = _createFish()
+			if fish_or_plastic == 2:
+				spawned = _createCrab()
 			add_child(spawned)
-			# Set speed and direction
-			spawned.set_linear_velocity(velocity)
 			spawned.position = Vector2(x,y)
 			if flipped:
+				spawned.set_linear_velocity(Vector2(-spawned.linear_velocity.x, -spawned.linear_velocity.y))
 				spawned.get_node("AnimatedSprite2D").set_flip_h(true);
 
 func _createPlastic():
 	var spawned
-	var selector = rng.randi_range(0,2)
+	var selector = rng.randi_range(0,3)
 	if selector == 0:
 		spawned = plastic1.instantiate()
+		spawned.set_meta("class","Plastic")
 	if selector == 1:
 		spawned = plastic2.instantiate()
+		spawned.set_meta("class","Plastic")
 	if selector == 2:
 		spawned = plastic3.instantiate()
-	spawned.set_meta("class","Plastic")
+		spawned.set_meta("class","Plastic")
+	if selector == 3:
+		spawned = knife.instantiate()
+		spawned.set_meta("class","Fish")
 	return spawned
 	
 func _createFish():	
 	var spawned
-	var selector = rng.randi_range(0,1)
+	var selector = rng.randi_range(0,2)
 	if selector == 0:
 		spawned = fish1.instantiate()
 	if selector == 1:
 		spawned = fish2.instantiate()
+	if selector == 2:
+		spawned = fish3.instantiate()	
 	spawned.set_meta("class","Fish")
 	return spawned
 	
+func _createCrab():	
+	var spawned = crab.instantiate()
+	spawned.set_meta("class","Fish")
+	return spawned
 		
 	
 	
